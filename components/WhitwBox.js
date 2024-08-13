209f076ad8b5fe60c207a6626dd9d95515c14ead
Khush5476 <patelkhush775@gmail.com>
2024-08-12 22:42:23 -0400

    dsada

--- components/WhitwBox.js
+++ components/WhitwBox.js
@@ -6,9 +6,13 @@ border-radius: 10px;
 padding: 30px;
 width: 280px;
 height: 320px;
-  @media  screen and (min-width: 768px){
+  @media  screen and (min-width: 975px){
 width: 550px;
 height: 550px;
+}
+  @media  screen and (min-width: 768){
+width: 450px;
+height: 450px;
 }
 
 
